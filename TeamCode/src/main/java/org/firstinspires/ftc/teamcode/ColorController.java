0b3db7b19106e0fab5491b7ea6fb1707f12a99f3
rishabh-bector <bector.rishabh@gmail.com>
2017-10-21 12:34:39 -0500

    Created motor controller and color sensor controller

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorController.java
@@ -0,0 +1,60 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.I2cAddr;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+/**
+ * This class manages color sensor methods.
+ */
+
+public class ColorController {
+
+    HardwareMap hardwareMap = null;
+    Telemetry telemetry;
+    ColorSensor colorSensor;
+
+    public ColorController(HardwareMap hardwareMap, Telemetry tel) {
+        colorSensor = hardwareMap.colorSensor.get("color");
+        colorSensor.setI2cAddress(I2cAddr.create8bit(0x3c));
+        telemetry = tel;
+    }
+
+    public void ledOn() {
+        colorSensor.enableLed(true);
+    }
+
+    public void ledOff() {
+        colorSensor.enableLed(false);
+    }
+
+    public String getColor() throws InterruptedException {
+        int redVal = colorSensor.red();
+        int blueVal = colorSensor.blue();
+        int greenVal = colorSensor.green();
+        int alphaVal = colorSensor.alpha();
+
+        Thread.sleep(20);
+
+        if(redVal > blueVal && redVal > greenVal) {
+            return "red";
+        }
+        if(blueVal > redVal && blueVal > greenVal) {
+            return "blue";
+        }
+
+        return "none";
+    }
+
+    public void printValues() {
+        telemetry.addData("Red", colorSensor.red());
+        telemetry.addData("Blue", colorSensor.blue());
+        telemetry.addData("Green", colorSensor.green());
+        telemetry.addData("Alpha", colorSensor.alpha());
+        telemetry.update();
+    }
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorController.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+/**
+ * Created by rishabhbector on 10/21/17.
+ */
+
+public class MotorController {
+
+    public DcMotor motor;
+
+    public MotorController(HardwareMap hardwareMap) {
+        motor = hardwareMap.get(DcMotor.class, "testMotor");
+    }
+
+    public void setPower(double power) {
+        motor.setPower(power);
+    }
+
+
+}
