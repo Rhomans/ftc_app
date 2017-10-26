b8d74c48df9ea6f28c0eeb726745642fc7b7e663
The_Senate <Nazucca7@gmail.com>
2017-10-25 19:10:12 -0500

    More work on the Vision. Almost ready for its own controler class.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VisionIdentification.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VisionIdentification.java
@@ -68,7 +68,7 @@ import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
 
 @Autonomous(name="Concept: VuMark Id", group ="Concept")
 @Disabled
-public class VisionIdentifacation extends LinearOpMode {
+public class VisionIdentification extends LinearOpMode {
 
     public static final String TAG = "Vuforia VuMark Sample";
 
